a3ffa546eedbb457fd69cc528115f3f3d437c23e
Carter Andrew <carterinmotion@gmail.com>
2025-04-13 14:04:24 -0600

    Script to make files in ff5 format for training

--- huggingface_dataset_builder.py
+++ huggingface_dataset_builder.py
@@ -5,22 +5,22 @@ from datasets import Dataset, Audio
 data_path = './data/audio_segments/'
 meta_name = 'meta.pkl'
 
-dataset_name = 'the-ramsay-expirience/ramsay_audio'
+dataset_name = 'the-ramsay-experience/ramsay_audio'
 
-meta_path = os.path.join(
-    data_path,
-    meta_name
-)
-if not os.path.exists(meta_path):
-    raise Exception(
-        "No metadata file found under audio_segments, missing dataset"
-    )
-
-df = pd.read_pickle(meta_path)
-
-ds = Dataset.from_pandas(df)
 
 if __name__ == '__main__':
+    meta_path = os.path.join(
+        data_path,
+        meta_name
+    )
+    if not os.path.exists(meta_path):
+        raise Exception(
+            "No metadata file found under audio_segments, missing dataset"
+        )
+
+    df = pd.read_pickle(meta_path)
+
+    ds = Dataset.from_pandas(df)
     audio_paths = []
     for index, row in df.iterrows():
         audio_paths.append(
