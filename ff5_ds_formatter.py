6096bedb286599c38384231ab8be4a89af6670a5
Carter <carterinmotion@gmail.com>
2025-04-16 10:41:18 -0600

    Removed f5 files for upload

--- ff5_ds_formatter.py
+++ ff5_ds_formatter.py
@@ -7,19 +7,20 @@ from tqdm import tqdm
 
 class FF5Formatter:
     dataset_dir = './dataset'
+    wavs_dir = os.path.join(dataset_dir, 'wavs')
     hf_ds_name = dataset_name
     def __init__(self):
         self.hf_ds = load_dataset(self.hf_ds_name)
         self.df = pd.DataFrame(self.hf_ds['train'])
-        os.makedirs(self.dataset_dir, exist_ok=True)
+        os.makedirs(self.wavs_dir, exist_ok=True)
         self.formatted_df = pd.DataFrame(
             columns=['audio_file', 'text']
         )
         filepaths = list()
         for i, row in tqdm(self.df.iterrows()):
             filename = f'{i}.wav'
-            filepath = os.path.join(self.dataset_dir, filename)
-            filepaths.append(filepath)
+            filepath = os.path.join(self.wavs_dir, filename)
+            filepaths.append(f'wavs/{filename}')
             soundfile.write(
                 filepath,
                 row['audio']['array'],
