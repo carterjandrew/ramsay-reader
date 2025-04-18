bddd9469f3350d529dbfabcaa3fd92d88296be7e
Carter <carterinmotion@gmail.com>
2025-04-18 18:19:30 +0000

    Fixed f5 exporter and added env file

--- f5_ds_formatter.py
+++ f5_ds_formatter.py
@@ -11,15 +11,15 @@ class FF5Formatter:
     def __init__(self):
         self.hf_ds = load_dataset(self.hf_ds_name)
         self.df = pd.DataFrame(self.hf_ds['train'])
-        os.makedirs(self.dataset_dir, exist_ok=True)
+        os.makedirs(self.dataset_dir + '/wavs', exist_ok=True)
         self.formatted_df = pd.DataFrame(
             columns=['audio_file', 'text']
         )
         filepaths = list()
         for i, row in tqdm(self.df.iterrows()):
             filename = f'{i}.wav'
-            filepath = os.path.join(self.dataset_dir, filename)
-            filepaths.append(filepath)
+            filepath = os.path.join(self.dataset_dir, 'wavs', filename)
+            filepaths.append(filename)
             soundfile.write(
                 filepath,
                 row['audio']['array'],
